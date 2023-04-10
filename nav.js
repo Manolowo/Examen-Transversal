1be10fe9b06963bd170f7561f975af4a757a2b5f
DiegoVelasquez <diego001vm@gmail.com>
2023-04-10 11:44:06 -0400

    10.04.23 Anticuario 1.5

--- nav.js
+++ nav.js
@@ -5,3 +5,7 @@ function openNav(){
 function closeNav(){
     document.getElementById("mobile_menu").style.width = "0%";
 }
+
+document.getElementById("button-addon2").onclick = function(){
+    alert("Correo recibido, le contactaremos en breves")
+}
