6a219eb3f50ded3d59978829b47f303a763bce73
DiegoVelasquez <diego001vm@gmail.com>
2023-04-11 16:35:15 -0400

    11.04.23 Anticuario v2.0

--- nav.js
+++ nav.js
@@ -9,6 +9,3 @@ function closeNav(){
 document.getElementById("button-addon2").onclick = function(){
     alert("Correo recibido, le contactaremos en breves")
 }
-document.getElementById("Comprar").onclick = function(){
-    alert("La página se redigirá al sitio web (WebPay) para proceder al pago")
-}
